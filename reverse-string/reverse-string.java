3feec807895809251b22e01f9e03954d807020c1
paravats123 <paravats@gmail.com>
2023-10-03 12:48:47 +0000

    Refactor codespace

--- reverse-string/reverse-string.java
+++ reverse-string/reverse-string.java
@@ -1,7 +1,7 @@
 class Solution {
     public void reverseString(char[] s) {
         int bPt = 0, ePt = s.length-1;
-        char bCh = Character.MIN_VALUE, tCh = Character.MIN_VALUE;
+        char tCh = Character.MIN_VALUE;
         while (bPt < ePt){
             
             tCh = s[bPt];
