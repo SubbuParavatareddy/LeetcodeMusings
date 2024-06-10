2579013886d3139da1fe976a1b182495f5389dad
paravats123 <paravats@gmail.com>
2024-06-09 19:40:43 -0700

    Time: 2 ms (86.36%), Space: 41.8 MB (35%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -1,30 +1,24 @@
 class Solution {
-    public int calPoints(String[] operations) {
-        int cap = operations.length;
-        int tot = 0, len = 0, lenRes = 0, temp = 0;
-        String[] res = new String[cap];
-        for (int i = 0; i < cap; i++) {
-            String val = operations[i];
-            if (val.equals("C")) {
-                res[lenRes-1] = "";
-                lenRes--;
-            } else if (val.equals("D")) {
-                temp = Integer.valueOf(res[lenRes-1]) * 2;
-                res[lenRes] = String.valueOf(temp);
-                lenRes++;
-            } else if (val.equals("+")) {
-                temp = Integer.valueOf(res[lenRes - 1]) + Integer.valueOf(res[lenRes - 2]);
-                res[lenRes] = String.valueOf(temp);
-                lenRes++;
-            } else {
-                res[lenRes] = val;
-                lenRes++;
+    public int calPoints(String[] ops) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int tot = 0;
+
+        for (String op: ops){
+            if (op.equals("+")){
+                int top = stack.pop();
+                int newtop = top + stack.peek();
+                stack.push(top);
+                stack.push(newtop);
+            }else if (op.equals("C")){
+                stack.pop();
+            }else if (op.equals("D")){
+                stack.push(stack.peek()*2);
+            }else{
+                stack.push(Integer.valueOf(op));
             }
         }
-        for (int i = 0; i < lenRes; i++) {
-            System.out.println(res[i]);
-            tot += Integer.valueOf(res[i]);
-        }
+        for(int score: stack) 
+            tot+= score;
         return tot;
     }
 }
