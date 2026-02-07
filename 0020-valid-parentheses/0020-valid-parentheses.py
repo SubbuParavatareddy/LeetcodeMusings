b0185ca32a479fb6bc1cd49ad11eae2a3981065d
Subbu Paravatareddy <20096350+SubbuParavatareddy@users.noreply.github.com>
2026-02-07 12:49:04 -0800

    Time: 0 ms (100%), Space: 19.4 MB (39.9%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,14 +1,14 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        matching_brackets = {'(' : ')', '{' : '}', '[' : ']'}
+        valid_pairs = { '(' : ')', '{' : '}', '[' : ']' }
         open_paren = set(['(', '{', '['])
-        stack = []
+        my_stack = []
 
         for ch in s:
             if ch in open_paren:
-                stack.append(ch)
-            elif len(stack) > 0 and ch == matching_brackets[stack[-1]]:
-                stack.pop()
+                my_stack.append(ch)
+            elif my_stack and ch == valid_pairs[my_stack[-1]]:
+                my_stack.pop()
             else:
                 return False
-        return stack == []
+        return my_stack == []
