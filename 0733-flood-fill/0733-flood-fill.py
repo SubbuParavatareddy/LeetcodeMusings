a9955e6cd302045badd26b13df4acde3931359fc
Subbu Paravatareddy <20096350+SubbuParavatareddy@users.noreply.github.com>
2026-02-14 15:29:33 -0800

    Time: 0 ms (100%), Space: 19.8 MB (20.56%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,20 +1,20 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        
-        org_color = image[sr][sc]
-        if org_color == color:
+        original_color = image[sr][sc]
+
+        if (image[sr][sc] == color):
             return image
 
-        def dfs(r, c):
-            if r < 0 or r >= len(image) or c < 0 or c >= len(image[0]) or image[r][c] != org_color:
+        def bfs(r, c):
+            if (r < 0) or (r >= len(image)) or (c < 0) or (c >= len(image[0])) or (image[r][c] != original_color): 
                 return
-
+            
             image[r][c] = color
 
-            dfs(r + 1, c)
-            dfs(r - 1, c)
-            dfs(r, c + 1)
-            dfs(r, c - 1)
-        
-        dfs(sr, sc)
+            bfs( r + 1, c )
+            bfs( r - 1, c )
+            bfs( r, c +1 )
+            bfs( r, c - 1 )
+
+        bfs(sr, sc)
         return image
